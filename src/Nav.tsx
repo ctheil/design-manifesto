f89be958b0ec6220bdd711a88bb779ee2b769cc5
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2023-09-11 20:42:17 -0600

    initial architecture

---
+++ src/Nav.tsx
@@ -0,0 +1,12 @@
+const Nav = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>Article 1</li>
+        <li>Article 2</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
