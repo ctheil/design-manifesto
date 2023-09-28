d5ad1c316af2d9e8dd5f208e4ef970091c82798c
Caleb Theil <86980706+ctheil@users.noreply.github.com>
2023-09-28 09:05:47 -0600

    ISSUE: eslint is not picking up issues that the builder issues FIX: typecasting should solve last build issue

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import Nav from "./Nav";
 import Article from "./components/Article";
 import Articles from "./assets/articles.json";
 import { Author } from "./components/Author";
+import { IArticle } from "./components/article.types";
 
 function App() {
   return (
     <section className="main">
-      <Nav articles={Articles} />
+      <Nav articles={Articles as IArticle[]} />
       {Articles.map((a, i) => {
         return (
           <Article
